7545fd47a2b0a50a1d2dc82a0c6f971067f9b80b
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-07-16 13:02:22 -0300

    Estudando isNaN()

---
+++ isNaN.js
@@ -0,0 +1,11 @@
+console.log(NaN == NaN) //NaN não é igual a nada,nem a si mesmo
+
+//Verificando se uma variável é NaN
+
+const teste = NaN
+
+if (isNaN(teste)) {
+    console.log('A variável teste é NaN')
+}
+
+
